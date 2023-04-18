f663ddb556d2cab14fc4a09eff017edcb0ab87e0
hsuwei <dwil5200@gmail.com>
2023-04-18 15:42:22 +0800

    feat: 調整檔案下載語法
    目的
      - 自動化下載所有資料表單
    
    項目
      1. 新增src\data\download_data.py，作為主要執行檔
      2. src\data\OpenData_Uni.py 備存，功能轉移完畢後刪除

---
+++ src/data/download_data.py
@@ -0,0 +1,49 @@
+# 下載資料檔
+
+import pandas as pd
+from pathlib import Path
+import requests
+from bs4 import BeautifulSoup
+import re
+
+
+# 專案路徑
+# TODO: 之後調整
+projRoot = Path(__file__).parents[2]
+root_rawdata = projRoot.joinpath('data/raw')
+
+# 資料種類
+dtcategory = ['學生類', '教職類', '研究類', '校務類', '財務類']
+
+
+dturl = 'https://udb.moe.edu.tw/udata/DetailReportList/'
+dturls = [dturl + c for c in dtcategory]
+
+# 檔案下載連結
+apdx = []
+
+for dturl in dturls:
+    r = requests.get(dturl)
+    soup = BeautifulSoup(r.text)
+    s1 = soup.find_all(attrs={'headers': 'csv'})
+    apdx0 = [ss.find('a').get('href') for ss in s1]
+    apdx.extend(apdx0)
+
+# 下載檔案
+try:
+    for url in apdx:
+        pattern = r'file/(.+)\.\s?[\u4e00-\u9fa5]'  # 中文字前面
+        # 有的還有空格
+        # 表單名稱
+        dtnm = re.search(pattern, url).group(1)
+        print(url)
+        response = requests.get('https://udb.moe.edu.tw/' + url)
+        fpth = root_rawdata.joinpath(dtnm + '.csv')
+
+        with open(fpth, 'wb') as f:
+            f.write(response.content)
+except:
+    print("An error occurred:", ExceptionType.__name__)
+
+if __name__ == '__main__':
+    print('Please source this file')
